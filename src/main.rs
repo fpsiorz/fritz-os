c9dd43d79be4d6759ae644c3e27385174e318631
Fritz Psiorz <fpz@posteo.de>
2018-03-31 21:21:09 +0200

    end of lesson one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+
+#[no_mangle]
+pub extern fn _start() -> ! {
+    loop {}
+}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn rust_begin_panic(_msg: core::fmt::Arguments, _file: &'static str, _line: u32, _column: u32) -> ! {
+    loop{}
 }
