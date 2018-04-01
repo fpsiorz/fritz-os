eb5e58d05fd9b2269ad35578c192f251f53f8a02
Fritz Psiorz <fpz@posteo.de>
2018-04-01 11:49:38 +0200

    lesson 2

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@
 #![no_main]
 #![feature(lang_items)]
 
+extern crate rlibc;
+
 #[no_mangle]
 pub extern fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &b) in b"Hello World".iter().enumerate() {
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = b;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
 
